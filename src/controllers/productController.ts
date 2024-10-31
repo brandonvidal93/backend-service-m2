fe17ca4db6a07288fd99fd311c0d00c712e71a2c
brandonvidal93 <brandonvidal93@gmail.com>
2024-10-31 12:04:34 -0500

    Actualización del Controlador

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -12,7 +12,7 @@ export const getAllProducts = async(req: Request, res: Response) => {
   } catch(error) {
     res.status(500).json({ message: "Error al obtener los productos."});
   }
-}
+};
 
 // Obtener (GET) un producto por ID
 export const getProductByID = async(req: Request, res: Response) => {
@@ -29,4 +29,72 @@ export const getProductByID = async(req: Request, res: Response) => {
   } catch(error) {
     res.status(500).json({ message: "Error al obtener el producto."});
   }
-}
+};
+
+// Crear (POST) un producto
+export const createProduct = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body; // Sacando los datos del Request
+    const product = new Product();
+    product.name = name;
+    product.description = description;
+    product.price = price;
+    await productRepository.save(product);
+    res.status(201).json(product);
+  } catch(error) {
+    res.status(500).json({
+      message: "Error al crear el producto."
+    });
+  }
+};
+
+// Actualizar (PUT) un producto
+export const updateProduct = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+
+    const product = await productRepository.findOneBy({
+      id: parseInt(req.params.id)
+    });
+
+    if (product) {
+      product.name = name ?? product.name;
+      product.description = description ?? product.description;
+      product.price = price ?? product.price;
+      await productRepository.save(product);
+      res.json(product);
+    } else {
+      res.status(404).json({
+        message: "Producto no encontrado."
+      });
+    }
+  } catch(error) {
+    res.status(500).json({
+      message: "Error al actualizar el producto."
+    });
+  }
+};
+
+// Borrar (DELETE) un producto
+export const deleteProduct = async(req: Request, res: Response) => {
+  try {
+    const product = await productRepository.findOneBy({
+      id: parseInt(req.params.id)
+    });
+
+    if (product) {
+      await productRepository.remove(product);
+      res.json({
+        message: "Producto eliminado."
+      });
+    } else {
+      res.status(404).json({
+        message: "Producto no encontrado."
+      });
+    }
+  } catch(error) {
+    res.status(500).json({
+      message: "Error al borrar el producto."
+    });
+  }
+};
