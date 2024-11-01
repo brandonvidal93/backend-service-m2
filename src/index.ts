21f234654ee93fd4ce20326f3bd6998cd70727d8
brandonvidal93 <brandonvidal93@gmail.com>
2024-11-01 12:43:57 -0500

    Ajustes en el config del server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { AppDataSource } from "./data-source";
-import swaggerUi from "swagger-ui-express";
+import router from "./routers/productRoutes";
+import swaggerUI from "swagger-ui-express";
+import swaggerSpec from "./swagger/swagger";
 
 const app: Application = express();
 const PORT = process.env.PORT ?? 3000;
@@ -11,3 +13,22 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
+app.use("/api/", router); // Ruta de Productos
+
+// Documentación Swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+// Inicialización de la base de datos y el servidor
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(() => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}\n`);
+      console.log(`Endpoints:`);
+      console.log(`API Products http://localhost:${PORT}/api/products\n`);
+      console.log(`Documentión:`);
+      console.log(`Swagger en http://localhost:${PORT}/api-docs`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+
